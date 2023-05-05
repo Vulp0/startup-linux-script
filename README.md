5747b9131cceff280be2e72c3e785659427701e6
Vulp0 <mauriciomartinezvaz@gmail.com>
2023-05-05 07:55:21 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# A simple script that opens VS Codium, Firefox, and Discord in one go
+And that's pretty much it
---
+++ Startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Small script that opens Firefox, VS Codium and Discord in one go
+
+nohup /home/vulpo/VSCodium-linux-x64-1.66.2/codium &
+nohup /usr/bin/firefox &
+nohup /home/vulpo/Desktop/uhh/Discord/Discord &
